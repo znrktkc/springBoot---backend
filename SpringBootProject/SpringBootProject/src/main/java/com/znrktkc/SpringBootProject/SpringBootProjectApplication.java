d966c54f4a93110461a238bddc21d0961fef75e5
znrktkc <znrktkc@hotmail.com>
2020-12-03 11:59:25 +0300

    initial commmit

---
+++ SpringBootProject/SpringBootProject/src/main/java/com/znrktkc/SpringBootProject/SpringBootProjectApplication.java
@@ -0,0 +1,14 @@
+package com.znrktkc.SpringBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("ss");
+		SpringApplication.run(SpringBootProjectApplication.class, args);
+	}
+
+}
